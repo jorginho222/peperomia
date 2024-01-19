f1dbfeffd9158eea8a6aa8ae024961eed95b3e7e
jorginho222 <rodizioperalta22@gmail.com>
2024-01-19 15:45:42 -0300

    Create first app. Start imc app

---
+++ app/src/main/java/com/ivanur/peperomia/imcCalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.ivanur.peperomia.imcCalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ivanur.peperomia.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
